bb1ff0ef70b11920d41827b8278d778a13c87774
justArale <just1arale@gmail.com>
2024-04-17 19:01:29 +0200

    added footer

---
+++ recipe-book-app/src/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer class="footer">
+      <h5 class="repo">
+        <a href="https://github.com/justArale/recipe-book-app">
+          GitHub Repository
+        </a>
+      </h5>
+    </footer>
+  );
+}
+
+export default Footer;
