ef64ec37d11d7f72d81acf1da307935f8cd84e7b
justArale <just1arale@gmail.com>
2024-04-17 19:12:48 +0200

    added sidebar and basic styling

--- recipe-book-app/src/App.jsx
+++ recipe-book-app/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./Navbar/Navbar";
+import Sidebar from "./Sidebar/Sidebar";
 import Footer from "./Footer/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Sidebar />
       <Footer />
     </>
   );
---
+++ recipe-book-app/src/Sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <ul class="sidebar">
+      <li class="home">
+        <a href="#">Home</a>
+      </li>
+      <li class="about">
+        <a href="#">About</a>
+      </li>
+    </ul>
+  );
+}
+
+export default Sidebar;
