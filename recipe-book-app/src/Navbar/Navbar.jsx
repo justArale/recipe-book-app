3e8eafd3dec99e49cd41d9557faeeb0c815f74d4
justArale <just1arale@gmail.com>
2024-04-17 18:49:46 +0200

    added navbar

---
+++ recipe-book-app/src/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav class="navbar">
+      <h1>Recipe Book</h1>
+      <img src="./src/assets/logo.jpg" alte="logo" />
+    </nav>
+  );
+}
+
+export default Navbar;
