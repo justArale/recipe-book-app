2194f1264972f29be787efb872490c484c911515
justArale <just1arale@gmail.com>
2024-04-19 16:53:49 +0200

    installed react-router, created error-page

--- recipe-book-app/src/App.jsx
+++ recipe-book-app/src/App.jsx
@@ -3,12 +3,19 @@ import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
 import List from "./components/List";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
-      <List />
+
+      <Routes>
+        <Route path="/" element={<List />} />
+
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+
       <Sidebar />
       <Footer />
     </>
