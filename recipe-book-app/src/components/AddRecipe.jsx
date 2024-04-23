2309084050d4cac9ecdf3b78b6c8e9208b35b94e
justArale <just1arale@gmail.com>
2024-04-23 21:33:26 +0200

    final style AddRecipePage

--- recipe-book-app/src/components/AddRecipe.jsx
+++ recipe-book-app/src/components/AddRecipe.jsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./AddRecipe.css";
 
 function AddRecipe(props) {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [file, setFile] = useState("");
-  const [ingredients, setIngredients] = useState("");
+  const [ingredientAmount, setIngredientAmount] = useState("");
+  const [ingredient, setIngredient] = useState("");
   const [instruction, setInstruction] = useState("");
 
   const handleNameInput = (e) => setName(e.target.value);
   const handleDescriptionInput = (e) => setDescription(e.target.value);
   const handleFileUpload = (e) =>
     setFile(URL.createObjectURL(e.target.files[0]));
-  const handleIngredientsInput = (e) => setIngredients(e.target.value);
+  const handleIngredientAmountInput = (e) =>
+    setIngredientAmount(e.target.value);
+  const handleIngredientInput = (e) => setIngredient(e.target.value);
   const handleInstructionInput = (e) => setInstruction(e.target.value);
 
   const handleSubmit = (e) => {
@@ -21,7 +25,8 @@ function AddRecipe(props) {
       name,
       description,
       file,
-      ingredients,
+      ingredientAmount,
+      ingredient,
       instruction,
     };
 
@@ -31,58 +36,85 @@ function AddRecipe(props) {
     setName("");
     setDescription("");
     setFile("");
-    setIngredients("");
+    setIngredientAmount("");
+    setIngredient("");
     setInstruction("");
   };
 
   return (
-    <div className="AddRecip">
-      <h4>Add your Recipe</h4>
+    <div>
+      <form className="addRecipe" onSubmit={handleSubmit}>
+        <div className="addRecipe-main">
+          <h4 className="addRecipe-header">Add your Recipe</h4>
+          <div className="addRecipe-headerRow">
+            <div className="addRecipe-headerContent">
+              <input
+                type="text"
+                name="name"
+                className="title"
+                placeholder="Title..."
+                value={name}
+                onChange={handleNameInput}
+              />
 
-      <form onSubmit={handleSubmit}>
-        <label>Name:</label>
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={handleNameInput}
-        />
-
-        <label>Description:</label>
-        <textarea
-          type="text"
-          name="description"
-          value={description}
-          onChange={handleDescriptionInput}
-        />
-
-        <label>Image:</label>
-        <input
-          type="file"
-          src={file}
-          alt={`${name}+Img`}
-          onChange={handleFileUpload}
-        />
-
-        <label>Ingredients:</label>
-        <input
-          type="text"
-          name="ingredients"
-          value={ingredients}
-          onChange={handleIngredientsInput}
-        />
-
-        <label>Instruction: </label>
-        <input
-          type="text"
-          name="instruction"
-          value={instruction}
-          onChange={handleInstructionInput}
-        />
-        <Link to="/">
-          <button type="delete">❌ Cancel</button>
-          <button type="submit">✅ Save</button>
-        </Link>
+              <textarea
+                type="text"
+                name="description"
+                className="description"
+                placeholder="Description..."
+                value={description}
+                onChange={handleDescriptionInput}
+              />
+            </div>
+            <div className="uploadButtonWrapper">
+              <label className="uploadButton">
+                <input
+                  type="file"
+                  src={file}
+                  alt={`${name}+Img`}
+                  onChange={handleFileUpload}
+                />{" "}
+                🖼️ Choose Image
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="addRecipe-ingredient">
+          <label className="ingredient-header">Ingredients</label>
+          <div className="ingredient-input">
+            <input
+              type="text"
+              name="ingredient-amount"
+              placeholder="1..."
+              value={ingredientAmount}
+              onChange={handleIngredientAmountInput}
+            />
+            <input
+              type="text"
+              name="ingredient"
+              placeholder="Ingredient..."
+              value={ingredient}
+              onChange={handleIngredientInput}
+            />
+          </div>
+        </div>
+        <div className="addRecipe-instruction">
+          <label className="instruction-header">Instruction: </label>
+          <input
+            className="instuction-input"
+            type="text"
+            name="instruction"
+            placeholder="Instruction..."
+            value={instruction}
+            onChange={handleInstructionInput}
+          />
+        </div>
+        <div className="action">
+          <Link to="/">
+            <button type="delete">❌ Cancel</button>
+            <button type="submit">✅ Save</button>
+          </Link>
+        </div>
       </form>
     </div>
   );
