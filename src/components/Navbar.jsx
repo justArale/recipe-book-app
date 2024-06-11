f53fb0661fc5b695dc822ed64fc23dc40d1398d8
justArale <just1arale@gmail.com>
2024-06-11 16:55:30 +0200

    set navbar-header as a link to the dashboard

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,20 @@
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="header">
       <div className="inner">
         <div className="nav-title">
-          <h1 className="nav-header">Recipe Book</h1>
+          <Link to={`/`} className="navbar-header-link">
+            <h1 className="nav-header">Recipe Book</h1>
+          </Link>
           <p className="nav-description">This is my awesome list of recipes.</p>
         </div>
         <div className="newRecipeNavigation">
-          <button className="addRecipe-button">
-            <a href="/newrecipe">New recipe</a>
-          </button>
+          <Link to={`/newrecipe`}>
+            <button className="addRecipe-button">New recipe</button>
+          </Link>
         </div>
       </div>
     </div>
