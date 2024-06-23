f43b85c1dc0f48557542c94fa858dc7c27465e4f
justArale <just1arale@gmail.com>
2024-06-23 11:54:02 +0200

    styling of placeholderimage and conditional rendering of classNames for the images

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,16 @@
 import "./List.css";
+import placeholderImage from "../assets/placeholder.svg";
 
 export default function List({ recipe }) {
   return (
     <div className="recipe-list">
       <div className="list-recipe">
         <div className="image-wrapper">
-          <img src={recipe.img} alt={recipe.Name} />
+          <img
+            src={recipe.img ? recipe.img : placeholderImage}
+            alt={recipe.Name}
+            className={`${recipe.img ? "image" : ""}`}
+          />
         </div>
         <div className="list-content">
           <div className="list-title">
