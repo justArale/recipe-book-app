75f5849634ffa6e4e59ceb7b11e3aa3da167f408
justArale <just1arale@gmail.com>
2024-07-02 22:24:23 +0200

    fetch author name to display

--- src/pages/AuthorRecipes.jsx
+++ src/pages/AuthorRecipes.jsx
@@ -1,8 +1,42 @@
 import React from "react";
 import DashboardPage from "./DashboardPage";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 function AuthorRecipes() {
-  return <DashboardPage />;
+  const { authorId } = useParams();
+  const [currentAuthor, setCurrentAuthor] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchAuthorData = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/api/user/${authorId}`);
+      setCurrentAuthor(response.data);
+      console.log("Author: ", response.data);
+    } catch (error) {
+      console.error("Error fetching author data:", error);
+      const errorDescription =
+        error.response?.data?.message ||
+        "An error occurred while fetching author data";
+      setErrorMessage(errorDescription);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchAuthorData();
+  }, []);
+
+  return (
+    <div>
+      <h2 className="headline">All recipe of: {currentAuthor.name}</h2>
+      <DashboardPage />
+    </div>
+  );
 }
 
 export default AuthorRecipes;
