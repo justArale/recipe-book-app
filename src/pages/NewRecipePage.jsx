815162f3d34217664aab2549e7d2f95a350b74ed
justArale <just1arale@gmail.com>
2024-07-02 16:47:41 +0200

    commented out unnecessary code because of skipping localhost

--- src/pages/NewRecipePage.jsx
+++ src/pages/NewRecipePage.jsx
@@ -3,27 +3,28 @@ import recipesData from "../components/recipes.json";
 import { useState, useEffect } from "react";
 
 function NewRecipePage() {
-  const [recipes, setRecipes] = useState(
-    JSON.parse(localStorage.getItem("recipes"))
-  );
-  const [recipesDataCopy, setRecipesDataCopy] = useState(
-    JSON.parse(localStorage.getItem("recipes"))
-  );
+  // const [recipes, setRecipes] = useState(
+  //   JSON.parse(localStorage.getItem("recipes"))
+  // );
+  // const [recipesDataCopy, setRecipesDataCopy] = useState(
+  //   JSON.parse(localStorage.getItem("recipes"))
+  // );
 
-  const addNewRecipe = (newRecipe) => {
-    const updatedRecipes = [...recipes, newRecipe];
-    const updatedRecipesDataCopy = [...recipesDataCopy, newRecipe];
+  // const addNewRecipe = (newRecipe) => {
+  //   const updatedRecipes = [...recipes, newRecipe];
+  //   const updatedRecipesDataCopy = [...recipesDataCopy, newRecipe];
 
-    setRecipes(updatedRecipes);
-    setRecipesDataCopy(updatedRecipesDataCopy);
-  };
+  //   setRecipes(updatedRecipes);
+  //   setRecipesDataCopy(updatedRecipesDataCopy);
+  // };
 
-  useEffect(() => {
-    // Update local storage whenever recipes state changes
-    localStorage.setItem("recipes", JSON.stringify(recipes));
-  }, [recipes]);
+  // useEffect(() => {
+  //   // Update local storage whenever recipes state changes
+  //   localStorage.setItem("recipes", JSON.stringify(recipes));
+  // }, [recipes]);
 
-  return <AddRecipe addRecipe={addNewRecipe} />;
+  // return <AddRecipe addRecipe={addNewRecipe} />;
+  return <AddRecipe />;
 }
 
 export default NewRecipePage;
