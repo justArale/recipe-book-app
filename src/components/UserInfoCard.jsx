73b864e27e21341510a5e706cd9f1e66688e6258
justArale <just1arale@gmail.com>
2024-07-07 14:11:45 +0200

    number of created recipes added to the user page

--- src/components/UserInfoCard.jsx
+++ src/components/UserInfoCard.jsx
@@ -7,24 +7,24 @@ function UserInfoCard({ author, user }) {
   return (
     <div className="userInfoBox">
       <div className="userInfo">
+        <div className="avatarImageWrapper">
+          <img
+            src={author.image || defaultImage}
+            alt={`${author.name}'s profile`}
+            className="userAvatarImage"
+          />
+        </div>
         <h1 className="pageTitle">{author.name}</h1>
         <div className="">
           <p className="body">
-            Description:{" "}
             {author.description ? author.description : "I'm a food lover."}
           </p>
 
           {user && author._id === user._id && (
             <p className="body">Email: {author.email}</p>
           )}
+          <p>{author.recipes.length} recipes</p>
         </div>
-      </div>
-      <div className="avatarImageWrapper">
-        <img
-          src={author.image || defaultImage}
-          alt={`${author.name}'s profile`}
-          className="userAvatarImage"
-        />
       </div>
     </div>
   );
