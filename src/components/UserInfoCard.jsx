cb12005d9d39c01498bfb13d597be0e5923712d9
justArale <just1arale@gmail.com>
2024-07-09 08:26:34 +0200

    set new bento-box styling for userInfoCard

--- src/components/UserInfoCard.jsx
+++ src/components/UserInfoCard.jsx
@@ -6,29 +6,33 @@ import "./UserInfoCard.css";
 function UserInfoCard({ author, user }) {
   return (
     <div className="userInfoBox">
+      <div className="userInfoWrapper">
+        <div className="userInfo">
+          <h1 className="pageTitle">{author.name}</h1>
+          <div className="">
+            <p className="mainFont">
+              {author.description ? author.description : "I'm a food lover."}
+            </p>
+
+            {user && author._id === user._id && (
+              <p className="mainFont">Email: {author.email}</p>
+            )}
+            <p></p>
+            <p
+              className="mainFont semiBoldWeigth"
+              style={{ marginTop: "auto" }}
+            >
+              {author.recipes?.length} recipes
+            </p>
+          </div>
+        </div>
+      </div>
       <div className="avatarImageWrapper">
         <img
           src={author.image || defaultAvatar}
           alt={`${author.name}'s profile`}
           className="userAvatarImage"
         />
-      </div>
-
-      <div className="userInfo">
-        <h1 className="pageTitle">{author.name}</h1>
-        <div className="">
-          <p className="mainFont">
-            {author.description ? author.description : "I'm a food lover."}
-          </p>
-
-          {user && author._id === user._id && (
-            <p className="mainFont">Email: {author.email}</p>
-          )}
-          <p></p>
-          <p className="mainFont thirdColor" style={{ marginTop: "auto" }}>
-            {author.recipes?.length} recipes
-          </p>
-        </div>
       </div>
     </div>
   );
