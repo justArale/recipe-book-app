3718a5cdd1b7b81982603abe6f4df700b1b044c4
justArale <just1arale@gmail.com>
2025-03-10 21:28:52 +0100

    Set token to root scope, move updateUser and changeUserPassword function to user.service.js #54

--- src/pages/UserEditPage.jsx
+++ src/pages/UserEditPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 import { AuthContext } from "../context/auth.context";
 import { useContext } from "react";
 // import fileUploadService from "../service/file-upload.service";
@@ -8,7 +8,11 @@ import {
   uploadAvatarImage,
   deleteAvatarImage,
 } from "../service/api/image.service";
-import { getSingleUser } from "../service/api/user.service";
+import {
+  getSingleUser,
+  updateUser,
+  changeUserPassword,
+} from "../service/api/user.service";
 import "../components/UserEditPage.css";
 import { Image } from "@just1arale/icons";
 import { Edit } from "@just1arale/icons";
@@ -17,7 +21,7 @@ import { Check } from "@just1arale/icons";
 import { Cancel } from "@just1arale/icons";
 import { extractPublicId } from "cloudinary-build-url";
 
-const API_URL = import.meta.env.VITE_API_URL;
+// const API_URL = import.meta.env.VITE_API_URL;
 
 const DEFAULT_USER_FORM_VALUES = {
   name: "",
@@ -32,7 +36,7 @@ function UserEditPage() {
   const { authorId } = useParams();
 
   const [formValues, setFormValues] = useState(DEFAULT_USER_FORM_VALUES);
-  const { user, authenticateUser } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   const [errorMessage, setErrorMessage] = useState(undefined);
   const [loading, setLoading] = useState(false);
   const [imageIsLoading, setImageIsLoading] = useState(false);
@@ -124,36 +128,40 @@ function UserEditPage() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const storedToken = localStorage.getItem("authToken");
+    // const storedToken = localStorage.getItem("authToken");
 
     try {
       if (formValues.oldPassword && formValues.newPassword) {
-        await axios.put(
-          `${API_URL}/api/user/${user._id}/change-password`,
-          {
-            oldPassword: formValues.oldPassword,
-            newPassword: formValues.newPassword,
-          },
-          {
-            headers: { Authorization: `Bearer ${storedToken}` },
-          }
+        await changeUserPassword(
+          user._id,
+          formValues.oldPassword,
+          formValues.newPassword
         );
+        // await axios.put(
+        //   `${API_URL}/api/user/${user._id}/change-password`,
+        //   {
+        //     oldPassword: formValues.oldPassword,
+        //     newPassword: formValues.newPassword,
+        //   },
+        //   {
+        //     headers: { Authorization: `Bearer ${storedToken}` },
+        //   }
+        // );
         setFormValues((prevValues) => ({
           ...prevValues,
           oldPassword: "",
           newPassword: "",
         }));
-        await authenticateUser();
         await fetchUserData();
         navigate(`/user/${authorId}`);
       } else {
         if (oldImageId) {
           deleteAvatarImage(oldImageId);
         }
-
-        await axios.put(`${API_URL}/api/user/${user._id}`, formValues, {
-          headers: { Authorization: `Bearer ${storedToken}` },
-        });
+        updateUser(user._id, formValues);
+        // await axios.put(`${API_URL}/api/user/${user._id}`, formValues, {
+        //   headers: { Authorization: `Bearer ${storedToken}` },
+        // });
 
         navigate(`/user/${authorId}`);
       }
--- src/service/api/user.service.js
+++ src/service/api/user.service.js
@@ -3,13 +3,16 @@ import axios from "axios";
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5005",
 });
+
+// Set token each time
 const token = localStorage.getItem("authToken");
+if (token) {
+  API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+}
 
 export const getAllUsers = async () => {
   try {
-    const response = await API.get(`api/user`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    const response = await API.get(`api/user`);
     return response;
   } catch (error) {
     console.error(error);
@@ -19,9 +22,7 @@ export const getAllUsers = async () => {
 
 export const getSingleUser = async (userId) => {
   try {
-    const response = await API.get(`/api/user/${userId}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    const response = await API.get(`/api/user/${userId}`);
     return response.data;
   } catch (error) {
     console.error(error);
@@ -29,11 +30,28 @@ export const getSingleUser = async (userId) => {
   }
 };
 
+export const updateUser = async (userId, values) => {
+  try {
+    await API.put(`/api/user/${userId}`, values);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const changeUserPassword = async (userId, oldPassword, newPassword) => {
+  try {
+    await API.put(`/api/user/${userId}/change-password`, {
+      oldPassword,
+      newPassword,
+    });
+  } catch (error) {
+    console.error("Error changing password:", error);
+  }
+};
+
 export const deleteUser = async (userId) => {
   try {
-    await API.delete(`/api/user/${userId}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    await API.delete(`/api/user/${userId}`);
   } catch (error) {
     console.error("Error deleting user:", error);
   }
@@ -42,5 +60,7 @@ export const deleteUser = async (userId) => {
 export default {
   getAllUsers,
   getSingleUser,
+  updateUser,
+  changeUserPassword,
   deleteUser,
 };
